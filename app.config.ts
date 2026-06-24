5dde862595f040fb4794495460b2649794f7ca45
oviey <omarvieyra@hotmail.com>
2026-06-24 12:51:44 -0600

    Fix: Actualizar .gitignore y añadir archivos esenciales

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from '@tanstack/react-start/config'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   server: {
-    preset: 'vercel',
-  },
-  vite: {
-    plugins: [
-      viteTsConfigPaths(),
-      tailwindcss(),
-    ],
+    preset: 'vercel', // Para desplegar en Vercel
   },
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    plugins: [
+        tsconfigPaths(),
+        tanstackStart(),
+        nitro(),
+        react(),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(process.cwd(), 'src'),
+        },
     },
-  },
 })
